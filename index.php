b4db66f207ac01f2e8c47d81e4a2202f0fce95c9
shidycool <124264897+shidycool@users.noreply.github.com>
2023-05-26 18:23:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel=""
+</head>
+<body>
+<div class="header">
+        <h1>order<span>processing</span>system</h1>
+      </div>
+      <br><br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br>
+        <hr>
+      <div class="links">
+        <li><a href=""></a></li>
+        <li><a href="..\dailystar_shoes.com\footware.html">Shoes</a></li>
+        <li><a href="..\infinitycreative.com\design.html">Designers</a></li>
+        <li><a href="..\contact\contact.html">Contact us</a></li>
+        <li><a href="#"></a></li>
+      </div>
+
+      <div>
+        <img src="cereal1.jpeg" alt="MAIZE">
+        <div>
+          <h3>
+            <span>Dried Maize</span>
+            <span>Ksh. 5,000</span>
+          </h3>
+          
+          
+        </div>
+      </div>
+  
+</body>
+</html>
