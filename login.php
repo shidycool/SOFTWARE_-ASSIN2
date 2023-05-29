d0fb23669f6fed8eb8349532fb99af044e1f36e3
shidycool <rashfrdd@gmail.com>
2023-05-29 16:59:10 +0300

    adding css file

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>login</title>
+  <link rel="stylesheet" href="signup.css">
 </head>
 <body>
   <h3>Order processing system</h3>
--- signup.php
+++ signup.php
@@ -7,14 +7,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="signup.css">
   <title>Signup</title>
 </head>
 <body>
-    <h3>Order processing system</h3>
+<div class="container">
+    <h3>Sign Up</h3>
     <form action="connection.php" method="POST">
       <fieldset>
+        
+        <div class="main- user-info">
+          <div class="user-input-box">
         <label for="username">Username: </label><br>
-        <input type="text" name="username" required>
+        <input type="text"placeholder="Full name" name="username" required>
+      </div>
+      </div>
       </fieldset>
 
       <fieldset>
@@ -24,7 +31,7 @@
 
       <fieldset>
         <label for="passwrd">Password: </label><br>
-        <input type="password"placeholder="username" name="passwrd" required>
+        <input type="password"placeholder="password" name="passwrd" required>
       </fieldset>
       
       <fieldset>
@@ -34,5 +41,6 @@
 
       <button type="submit">SUBMIT</button>
     </form>
+</div>
 </body>
 </html>
